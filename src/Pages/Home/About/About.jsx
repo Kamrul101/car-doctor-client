7af1d4a7cb8a07da1e8b68ab47b95dde1318397b
unknown <Kamrul101>
2023-05-18 13:52:15 +0600

    client

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import person from '../../../assets/images/about_us/person.jpg'
+import parts from '../../../assets/images/about_us/parts.jpg'
+
+const About = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200 mt-5">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="lg:w-1/2 relative">
+        <img
+          src={person}
+          className="w-3/4 rounded-lg shadow-2xl"
+        />
+        <img
+          src={parts}
+          className="w-1/2 rounded-lg shadow-2xl absolute right-12 top-1/2 border-8 border-white"
+        />
+        </div>
+        <div className="lg:w-1/2">
+            <p className="text-2xl font-semibold text-orange-600 mb-4">About us</p>
+          <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+          <p className="py-6 text-gray-400">
+          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomized words which don't look even slightly believable. 
+          </p>
+          <p className="py-6 text-gray-400">
+          the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.  
+          </p>
+          <button className="btn btn-error text-white">Get more info</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(()=>{
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+    return (
+        <div className='bg-gray-100 p-4'>
+            <div className='text-center'>
+                <h1 className='text-3xl text-orange-600 font-bold'>Our Services</h1>
+                <h1 className='text-5xl'>Our Service Area</h1>
+                <p>
+                the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                </p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6'>
+                
+                {
+                    services.map(service => <ServiceCard
+                    key={service._id}
+                    service={service}
+                    ></ServiceCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
