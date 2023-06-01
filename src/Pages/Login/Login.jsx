0664b76cadd2aac4268a460ba7d923f6d620b80a
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2023-06-01 14:31:29 +0600

    update

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,14 @@
 import React, { useContext } from "react";
 import img from '../../assets/images/login/login.svg'
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
     const {signIn}= useContext(AuthContext);
+    const location = useLocation();
+
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || '/';
     const handleLogin = event =>{
         event.preventDefault();
         const form = event.target;
@@ -18,6 +22,7 @@ const Login = () => {
         })
         .then(error=>{
             console.log(error);
+            navigate(from,{replace: true})
         })
 
     }
@@ -47,7 +52,7 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
                 placeholder="password"
                 name="password"
                 className="input input-bordered"
--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,16 +1,17 @@
 import React, { Children, useContext } from 'react';
 import { AuthContext } from '../Providers/AuthProvider';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivateRoute = ({children}) => {
     const {user, loading} = useContext(AuthContext);
+    const location = useLocation();
     if(loading){
         <progress className="progress progress-error w-56"></progress>
     }
     if(user?.email){
         return children;
     }
-    return <Navigate to="/login" replace></Navigate>
+    return <Navigate to="/login" state={{from: location}} replace></Navigate>
 };
 
 export default PrivateRoute;
